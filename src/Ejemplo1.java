99e5b07dffd23290ec0afde6ff8289dd3e9f0669
María José <mjibanezsl@gmail.com>
2022-04-25 12:09:05 +0200

    Añado el pepe

--- src/Ejemplo1.java
+++ src/Ejemplo1.java
@@ -2,6 +2,7 @@ public class Ejemplo1 {
     public static void main(String[] args) {
         System.out.println("hola");
         System.out.println("adios");
+        System.out.println("pepe");
     }
 
 
